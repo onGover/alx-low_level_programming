05377ba1d9759e58d980618f53bef0003b5c5f82
onGover <oabdulsamad4@gmail.com>
2022-09-08 20:27:51 -0700

    corleone

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - print string in the put function
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+
+}
