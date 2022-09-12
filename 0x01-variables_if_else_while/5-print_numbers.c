bc13b0bacd5be10ceef60632e4a259a2887f0fc6
onGover <oabdulsamad4@gmail.com>
2022-09-12 15:32:33 -0700

    goat

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	char n;
 
-	for (n='0'; n <= '9'; n++)
+	for (n ='0'; n < = '9'; n++)
 	{
 		putchar (n);
 	}
 
-	putchar('\n')
+	putchar ('\n');
 	
-	return(0);
+	return (0);
 }
