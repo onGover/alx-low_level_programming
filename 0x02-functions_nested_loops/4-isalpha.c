c89f9215b94ddfc2590f32d152022dcd962d8377
onGover <oabdulsamad4@gmail.com>
2022-09-14 19:12:46 -0700

    great

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - function to check if c is a letter, lowercase or uppercase
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
