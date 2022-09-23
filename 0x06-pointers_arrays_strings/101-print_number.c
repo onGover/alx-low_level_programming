a5b5ba3a0073e0d2c263c850fd64cf1016d236c8
onGover <oabdulsamad4@gmail.com>
2022-09-23 14:36:34 -0700

    number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,30 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
-
-int main(void)
+/**
+* print_number - prints an integer
+*@n:integer to be printed
+*
+*/
+void print_number(int n)
 {
+	unsigned int num = n;
 
-int n;
-int a[5];
-int *p;
+	if (n < 0)
+	{
+		_putchar(*-*);
+		num = -num;
+	}
 
-a[2] = 1024;
-p = &n;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-
-*(p + 5) = 98;
-
-  /* ...so that this prints 98\n */
-
-printf("a[2] = %d\n", a[2]);
-
-return (0);
+	_putchar((num % 10) + '0');
 }
--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,9 +10,7 @@ int a[5];
 int *p;
 
 
-
 a[2] = 1024;
-
 p = &n;
 
   /*
@@ -23,6 +21,7 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
+
 *(p + 5) = 98;
 
   /* ...so that this prints 98\n */
